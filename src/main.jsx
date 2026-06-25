a5568d24be3c9001ddf35c97c837a163a746b214
German Quintana <germanquintana@MacBookAir.lan>
2026-06-25 21:26:48 +0200

    Add Sentry error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from "@sentry/react";
 import './index.css'
 import App from './App.jsx'
+
+Sentry.init({
+  dsn: "https://358f99e4708537180c6bf529dfcd79c4@o4511627689721856.ingest.de.sentry.io/4511627696537680",
+  environment: import.meta.env.MODE,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({ maskAllText: false, blockAllMedia: false }),
+  ],
+  tracesSampleRate: 0.2,
+  replaysSessionSampleRate: 0,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
